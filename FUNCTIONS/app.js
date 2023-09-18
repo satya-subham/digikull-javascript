37a84e8b9a12bda05d2dd065395862653f7a58da
satya-subham <satyasubham85@gmail.com>
2023-09-18 11:37:44 +0530

    functions

---
+++ FUNCTIONS/app.js
@@ -0,0 +1,44 @@
+// let arr = [35, 37, 40, 51, 65, 100, 89];
+
+// function isEven(num) {
+//    return num % 2 === 0;
+// }
+// for(let counter = 0; counter < arr.length; counter++) {
+//     let isNumberEven = isEven(arr[counter]);
+//     console.log(isNumberEven);
+// }
+
+// const obj = {
+//     a: 2,
+//     b: (p, q) => {
+//         return this
+    
+//     }
+// }
+// let res = obj.b(1, 2)
+// console.log(res);
+
+
+// let obj = {
+//     a: 1,
+//     b:2
+// }
+// const fun = () =>{
+//     console.log(this);
+// }
+// fun();
+
+
+
+
+
+
+
+
+
+// let str = "eye";
+// for(let i = 0; i < str.length; i++) {
+//    if(str.indexOf(i) == str.lastIndexOf(i)){
+//     console.log(str[i]);
+//    }
+// }
