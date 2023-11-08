2e01a31afc4827db35df87e69fd60d5e6ace9680
satya-subham <satyasubham85@gmail.com>
2023-11-08 11:59:31 +0530

    quick sort

--- DSA/quickSort.js
+++ DSA/quickSort.js
@@ -1,31 +1,53 @@
-function partition(arr, low, high) {
-  let pivot = arr[high];
-  let i = low - 1;
+// function partition(arr, low, high) {
+//   let pivot = arr[high];
+//   let i = low - 1;
 
-  for (let j = low; j <= high - 1; j++) {
-    if (arr[j] < pivot) {
-      i++;
-      [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
+//   for (let j = low; j <= high - 1; j++) {
+//     if (arr[j] < pivot) {
+//       i++;
+//       [arr[i], arr[j]] = [arr[j], arr[i]];
+//     }
+//   }
+
+//   [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
+//   return i + 1;
+// }
+
+// function quickSort(arr, low, high) {
+//   if (low < high) {
+//     let pi = partition(arr, low, high);
+
+//     quickSort(arr, low, pi - 1);
+//     quickSort(arr, pi + 1, high);
+//   }
+// }
+
+// let arr = [10, 7, 8, 9, 1, 5];
+// let N = arr.length;
+
+// quickSort(arr, 0, N - 1);
+// console.log("Sorted array:");
+// console.log(arr);
+// console.log(arr.join(" "));
+
+
+
+// ========================================== ANOTHER ALGORITHM =================================
+
+function quickSort(arr){
+  if (arr.length <= 1) return arr;
+  
+  let pivot = arr[0];
+  let left = [];
+  let right = [];
+  for (let i = 1; i < arr.length; i++) {
+    if(arr[i] < pivot) {
+      left.push(arr[i])
+    }else {
+    right.push(arr[i])
+  };
   }
 
-  [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
-  return i + 1;
+  return [...quickSort(left), pivot ,...quickSort(right)];
 }
-
-function quickSort(arr, low, high) {
-  if (low < high) {
-    let pi = partition(arr, low, high);
-
-    quickSort(arr, low, pi - 1);
-    quickSort(arr, pi + 1, high);
-  }
-}
-
-let arr = [10, 7, 8, 9, 1, 5];
-let N = arr.length;
-
-quickSort(arr, 0, N - 1);
-console.log("Sorted array:");
-console.log(arr);
-console.log(arr.join(" "));
+console.log(quickSort([10, 7, 8, 9, 1, 5]));
