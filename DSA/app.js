2c94b505d1acd691711da260d40d13e43a7c80b1
satya-subham <satyasubham85@gmail.com>
2023-11-08 10:19:26 +0530

    mergesort

--- DSA/app.js
+++ DSA/app.js
@@ -1,44 +1,73 @@
-function mergeSort(arr, start, end) {
-  if(start >= end) return;
-  let mid = Math.floor((start+end)/2);
-  mergeSort(arr, start, mid);
-  mergeSort(arr, mid+1, end);
-  merge(arr, start, mid, end);
-}
-function merge(arr, start, mid, end) {
-  let resArray = [];
-  let len1 = mid - start + 1;
-  let len2 = end - mid;
-  let i = 0, j = 0;
-  while(i<len1 && j < len2) {
-    let elementA = arr[start+i];
-    let elementB = arr[mid+1+j];
-    if(elementB < elementA) {
-      resArray.push(elementB);
-      j++;
-    }
-    else {
-      resArray.push(elementA);
-      i++;
-    }
- }
+// function mergeSort(arr, start, end) {
+//   if(start >= end) return;
+//   let mid = Math.floor((start+end)/2);
+//   mergeSort(arr, start, mid);
+//   mergeSort(arr, mid+1, end);
+//   merge(arr, start, mid, end);
+// }
 
-  while(i<len1) {
-    resArray.push(arr[start+i]);
-    i++;
-  }
+// const arr = [1, 3, 0, 4, 5, 1]
+// mergeSort(arr, 0, arr.length-1);
+// console.log(arr);
 
-  while(j<len2) {
-    resArray.push(arr[mid+1+j]);
-    j++;
-  }
+// function merge(arr, start, mid, end) {
+//   let resArray = [];
+//   let len1 = mid - start + 1;
+//   let len2 = end - mid;
+//   let i = 0, j = 0;
+//   while(i<len1 && j < len2) {
+//     let elementA = arr[start+i];
+//     let elementB = arr[mid+1+j];
+//     if(elementB < elementA) {
+//       resArray.push(elementB);
+//       j++;
+//     }
+//     else {
+//       resArray.push(elementA);
+//       i++;
+//     }
+//  }
+
+//   while(i<len1) {
+//     resArray.push(arr[start+i]);
+//     i++;
+//   }
+
+//   while(j<len2) {
+//     resArray.push(arr[mid+1+j]);
+//     j++;
+//   }
 
  
-  for(let i=start; i<=end; i++) {
-    arr[i] = resArray[i-start];
+//   for(let i=start; i<=end; i++) {
+//     arr[i] = resArray[i-start];
+//   }
+
+// }
+
+
+
+// ========================== ANOTHER FUNCTIONS =========================
+
+function mergeSort(arr){
+  if(arr.length <= 1) return arr;
+
+  let mid = Math.floor(arr.length/2);
+
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+function merge(left, right){
+  let sortedArr = [];
+
+  while (left.length && right.length){
+    if(left[0] < right[0]) sortedArr.push(left.shift());
+    else sortedArr.push(right.shift());
   }
 
+  return [...sortedArr, ...left, ...right];
 }
-const arr = [1, 3, 0, 4, 5, 1]
-mergeSort(arr, 0, arr.length-1);
-console.log(arr);
+
+console.log(mergeSort([1, 3, 0, 4, 5, 1]))
