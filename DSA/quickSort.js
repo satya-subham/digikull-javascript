7718c67c5b67653794d5dba7b89de60575d95b73
satya-subham <satyasubham85@gmail.com>
2023-11-17 10:34:41 +0530

    quicksort

--- DSA/quickSort.js
+++ DSA/quickSort.js
@@ -51,3 +51,25 @@ function quickSort(arr){
   return [...quickSort(left), pivot, ...quickSort(right)];
 }
 console.log(quickSort([10, 7, 8, 9, 1, 4, 5, 2, 6, 3]));
+
+
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  
+  let pivot = arr[arr.length - 1];
+  let left = [];
+  let right = [];
+  
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+console.log(quickSort([0, 2, 1, 2, 0]))
