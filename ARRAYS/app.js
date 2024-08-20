abe9b0bac0fa96724f4bacb691bd9ac144a23afb
satya-subham <satyasubham85@gmail.com>
2024-08-20 12:47:03 +0530

    practice

--- ARRAYS/app.js
+++ ARRAYS/app.js
@@ -1,11 +1,21 @@
-let arr = [1, 2, 3, 4, 5];
+// let arr = [1, 2, 3, 4, 5];
 
 // let res = arr.map((item, index, arr) => {
 //     return item*2
 // });
 
-let res = arr.filter((item, index, arr) =>{
-    return item%2 === 0;
-})
+// let res = arr.filter((item, index, arr) =>{
+//     return item%2 === 0;
+// })
 
-console.log(res, arr);
+// console.log(res, arr);
+
+
+function addSub(x, y){
+    let c = x + y;
+    let d = x - y;
+    return {c, d}
+}
+let { c, d } = addSub(10, 5);
+console.log(c, d);
+
---
+++ DOM/app.js
@@ -0,0 +1,7 @@
+const data = [1, "sid", 5, "sim", 3, 6, 8, 9, 10, "apple"];
+
+data.map((num)=>{
+    if(!isNaN(num)){
+        console.log(num);
+    }
+})
